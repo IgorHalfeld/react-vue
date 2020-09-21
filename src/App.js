ba725fdae446bb3a9fbf53d556469b6a9df4181d
Igor Halfeld <hello@igorluiz.me>
2020-09-21 15:57:04 -0300

    the braba was added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { reactive, watch } from '@vue/runtime-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function useReactiveState (obj) {
+  const [, tick] = useState(0)
+
+  const [state] = useState(() => {
+    const state = reactive(obj)
+    watch(state, () => tick(v => !v))
+    return state
+  })
+
+  return state
 }
 
-export default App;
+function App () {
+  const state = useReactiveState({ x: 0, y: 0 })
+
+  function handleMouvemove (event) {
+    state.x = event.clientX
+    state.y = event.clientY
+  }
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouvemove)
+
+    return () => window.removeEventListener('mousemove', handleMouvemove)
+  })
+
+  return (
+    <div style={{ position: 'absolute', width: '100%', height: '100%' }}>
+      <img
+        style={{
+          position: 'absolute',
+          top: `${state.y - 100}px`,
+          left: `${state.x - 80}px`
+        }}
+        width="150"
+        src="evan.png"
+        alt="exanzin" />
+
+      <pre style={{ position: 'absolute', right: '30px', top: '0px' }}>
+        x: {state.x}px - y: {state.y}px
+      </pre>
+    </div>
+  )
+}
+
+export default App
